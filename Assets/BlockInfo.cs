ccd415f613d7c38e65db9230e12d8c06ddd5b12a
hktrollgames <hktrollgames@gmail.com>
2021-07-19 20:39:32 +0900

    카메라 이동함

--- Assets/BlockInfo.cs
+++ Assets/BlockInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +11,20 @@ public enum BlockType
 public class BlockInfo : MonoBehaviour
 {
     public BlockType blockType;
+
+    Vector3 downMousePosition;
+    public float clickDistance = 1;
     void OnMouseDown()
     {
+        downMousePosition = Input.mousePosition;
+    }
+    void OnMouseUp()
+    {
+        var upMousePosition = Input.mousePosition;
+        if (Vector3.Distance(downMousePosition, upMousePosition) > clickDistance)
+        {
+            return;
+        }
         GroundManager.Instance.OnTouch(transform.position);
     }
 }
---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : SingletonMonoBehavior<FollowTarget>
+{
+    Transform target;
+    public Vector3 offset = new Vector3(0, 0, -7);
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null)
+            return;
+
+        var newPos = target.position + offset;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+}

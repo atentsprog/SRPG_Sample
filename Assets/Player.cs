fdc466ed454e65ee8646e7b53c5c57c616a0a873
hktrollgames <hktrollgames@gmail.com>
2021-07-20 12:35:48 +0900

    플레이어 이동시 맵정보 갱신

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -21,18 +21,25 @@ public class Player : MonoBehaviour
 
     public BlockType passableValues;
 
-    internal void MoveTo(Vector2Int toIntVector)
+    internal void MoveTo(Vector2Int destination)
     {
         Dictionary<Vector2Int, BlockType> map = GroundManager.Instance.map;
 
         var beginPos = transform.position.ToIntVector2();
-        List<Vector2Int> path = PathFinding2D.Astar(beginPos, toIntVector, map, passableValues);
-
+        List<Vector2Int> path = PathFinding2D.Astar(beginPos, destination, map, passableValues);
+        if (path.Count == 0)
+        {
+            Debug.Log("길이 없다");
+            return;
+        }
+        
         StartCoroutine(MoveToCo(path));
     }
 
     private IEnumerator MoveToCo(List<Vector2Int> path)
     {
+        var startPos = path[0];
+        GroundManager.Instance.map[startPos] &= ~BlockType.Player; // Bit연산 값 빼기
         PlayAnimation("Walk");
         FollowTarget.Instance.SetTarget(Player.SelectPlayer.transform);
         foreach (var item in path)
@@ -45,6 +52,9 @@ public class Player : MonoBehaviour
         }
         PlayAnimation("Idle");
         FollowTarget.Instance.SetTarget(null);
+
+        var destination = path[path.Count - 1];
+        GroundManager.Instance.map[destination] |= BlockType.Player;// Bit연산 값 더하기
     }
     public Ease moveEase = Ease.Linear;
     public float moveTimePerUnit = 0.3f;
